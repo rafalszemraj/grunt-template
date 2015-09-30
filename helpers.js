3412f3c836ac4da967126a190fc252faa2cbc132
Rafał Szemraj <rafael.szemraj@gmail.com>
2015-09-30 17:30:27 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -8,12 +8,18 @@ function getElement( d3element, elementSelector) {
 
 export default {
 
+  // object helpers
   getPath               : R.curry((path,object) => R.path( R.split('.', path), object )),
   addTo                 : R.curry((object, property, value) => R.assoc( property, value, object) ),
   emptyObjectOr         : R.defaultTo({}),
   emptyListOr           : R.defaultTo([]),
   hasDefinedProps       : R.curry((propsPaths, object) => R.allPass( R.map( getPath, propsPaths ), object )),
+
+  // alt action helpers
   actionDispatcher      : (altAction, transform = R.identity) => R.compose( altAction.dispatch.bind(altAction), transform),
+  passDataToAction      : R.curry(( action, transform = R.identity ) => R.compose( action, transform ) ),
+
+  // d3 helpers
   getElement
 }
 
