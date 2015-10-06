b5dd989a61b2e95b31a4a1f9ba082a12f86887b4
Rafał Szemraj <rafael.szemraj@gmail.com>
2015-10-06 16:51:24 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,26 +1,42 @@
 import R from 'ramda'
 
-function getElement( d3element, elementSelector) {
-
-    var element = d3element.select(elementSelector);
-    return element.empty() ? d3element.append(elementSelector) : element;
-}
+const getPath = R.curry((path,object) => R.path( R.split('.', path), object ))
 
 export default {
 
   // object helpers
-  getPath               : R.curry((path,object) => R.path( R.split('.', path), object )),
+  getPath,
   addTo                 : R.curry((object, property, value) => R.assoc( property, value, object) ),
   emptyObjectOr         : R.defaultTo({}),
   emptyListOr           : R.defaultTo([]),
   hasDefinedProps       : R.curry((propsPaths, object) => R.allPass( R.map( getPath, propsPaths ), object )),
+  getPropOr             : R.curry((path, def) => R.ifElse( R.isNil, R.always(def), R.compose( R.defaultTo(def), getPath(path)) )),
 
   // alt action helpers
   actionDispatcher      : (altAction, transform = R.identity) => R.compose( altAction.dispatch.bind(altAction), transform),
   passDataToAction      : R.curry(( action, transform = R.identity ) => R.compose( action, transform ) ),
 
+    // async
+  dispatchAsyncCall     : (promiseOrFunction, name) => {
+
+    let promise = R.is(Promise, promiseOrFunction ) ? promiseOrFunction : promiseOrFunction();
+    actions.onAsyncOperation({name, promise});
+    return () => promise;
+
+  },
+
+  // components
+  dim                   : R.pick(['width', 'height']),
+  classed               : R.compose( R.join(' '), R.keys, R.pickBy( R.identity )),
+
+
+
   // d3 helpers
-  getElement
+  getElement            : (d3element, elementSelector) => {
+
+    var element = d3element.select(elementSelector);
+    return element.empty() ? d3element.append(elementSelector) : element;
+  }
 }
 
 
